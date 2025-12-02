11c2b0aad456f79261088ed9f11d2d1db5a76ce7
Phan Vinh Loc <77631348+locphan201@users.noreply.github.com>
2025-12-02 09:35:51 +0700

    Add files via upload

---
+++ nodes_torch_compile.py
@@ -0,0 +1,53 @@
+from typing_extensions import override
+from comfy_api.latest import ComfyExtension, io
+from comfy_api.torch_helpers import set_torch_compile_wrapper
+
+class TorchCompileModel(io.ComfyNode):
+    @classmethod
+    def define_schema(cls) -> io.Schema:
+        return io.Schema(
+            node_id="TorchCompileModel",
+            category="_for_testing",
+            inputs=[
+                io.Model.Input("model"),
+                io.Combo.Input(
+                    "backend",
+                    options=["inductor", "cudagraphs"],
+                ),
+            ],
+            outputs=[io.Model.Output()],
+            is_experimental=True,
+        )
+
+    @classmethod
+    def execute(cls, model, backend) -> io.NodeOutput:
+        m = model.clone()
+        compile_kwargs = {
+            "mode": None,
+            "fullgraph": True,
+            "dynamic": False,
+            "options": {
+                "max_autotune": True,
+                "max_autotune_gemm": True,
+                "triton.cudagraphs": False,
+                "shape_padding": True,
+                "epilogue_fusion": True,
+                "max_autotune_gemm_backends": "TRITON,CUTLASS",
+                "fx_graph_cache": True,
+                "autotune_local_cache": True,
+            }
+        }
+        set_torch_compile_wrapper(model=m, backend=backend **compile_kwargs)
+        return io.NodeOutput(m)
+
+
+class TorchCompileExtension(ComfyExtension):
+    @override
+    async def get_node_list(self) -> list[type[io.ComfyNode]]:
+        return [
+            TorchCompileModel,
+        ]
+
+
+async def comfy_entrypoint() -> TorchCompileExtension:
+    return TorchCompileExtension()
